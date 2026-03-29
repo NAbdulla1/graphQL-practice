51d10b0a98b6a506e468f6db8a170e31053d3434
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2026-03-29 19:37:28 +0600

    feat: integrate graphql-constraint-directive and apply validation constraints to mutation arguments

--- src/schema.ts
+++ src/schema.ts
@@ -2,10 +2,13 @@ import { createSchema } from 'graphql-yoga'
 import { resolvers } from './schema/resolvers.generated'
 import { typeDefs } from './schema/typeDefs.generated'
 import { urlDirective } from './schema/directives/urlDirective'
+import { constraintDirective, constraintDirectiveTypeDefs } from 'graphql-constraint-directive'
 
 const executableSchema = createSchema({
   resolvers: [resolvers],
-  typeDefs: [typeDefs]
+  typeDefs: [constraintDirectiveTypeDefs, typeDefs]
 })
 
-export const schema = urlDirective(executableSchema)
+const constraint = constraintDirective()
+
+export const schema = constraint(urlDirective(executableSchema))
